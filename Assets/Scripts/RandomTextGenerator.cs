484e5fcc382bf9a6ce695ddbe67b3b64458159d8
Ayush Kumar <ayushrocks456@gmail.com>
2020-12-02 22:41:10 +0530

    Random Text Generation

---
+++ Assets/Scripts/RandomTextGenerator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+public class RandomTextGenerator
+{
+    const string glyphs = "abcdefghijklmnopqrstuvwxyz";
+    string wordGenerated = null;
+    public string GenerateWord(int numberOfWords)
+    {
+        for(int i = 0; i < numberOfWords; i++)
+        {
+            int randomIndex = Random.Range(0, 26);
+            wordGenerated += glyphs[randomIndex];
+            Debug.Log(randomIndex + " " +  wordGenerated);
+        }
+        return wordGenerated;
+        
+    }
+}
